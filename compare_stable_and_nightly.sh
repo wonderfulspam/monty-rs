5b32fa38d86129cc600b8dc6f377cd45319d032d
Emil Thorenfeldt <emt@magenta-aps.dk>
2020-09-08 23:12:15 +0200

    Clean up comparison script

--- compare_stable_and_nightly.sh
+++ compare_stable_and_nightly.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Small script for comparing binaries compiled with nightly
+# and stable.
+
 # Helper function for checking for binaries
 function test_bin_exists () {
     BIN="$1"
@@ -9,10 +12,19 @@ function test_bin_exists () {
 test_bin_exists cargo
 test_bin_exists hyperfine
 
-echo "Building nightly version"
-cargo +nightly build -q --release
-cp ./target/release/monty-rs /tmp/monty-rs-nightly
-echo "Building stable version"
+echo -n "Building using stable toolchain:  "
+rustc +stable --version
 cargo +stable build -q --release
-echo "Running benchmarks"
-hyperfine /tmp/monty-rs-nightly ./target/release/monty-rs
+cp ./target/release/monty-rs /tmp/monty-rs-stable
+echo -n "Building using nightly toolchain: "
+rustc +nightly --version
+cargo +nightly build -q --release
+echo "-----------------------------------------"
+hyperfine /tmp/monty-rs-stable ./target/release/monty-rs
+# Due to the full utilisation of all available cores,
+# CPU clock speed may be throttled to prevent overheating.
+# Re-running in reverse order should prove that this isn't
+# a fluke
+echo "-----------------------------------------"
+echo "Re-running benchmarks in opposite order"
+hyperfine ./target/release/monty-rs /tmp/monty-rs-stable
